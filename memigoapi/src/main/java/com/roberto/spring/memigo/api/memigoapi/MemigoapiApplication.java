58a1165de9775f51a7187e4da3dd47b14a05aca1
RobertoRedes2001 <romaav@floridauniversitaria.es>
2024-04-20 13:27:56 +0200

    comit api inicial

---
+++ memigoapi/src/main/java/com/roberto/spring/memigo/api/memigoapi/MemigoapiApplication.java
@@ -0,0 +1,13 @@
+package com.roberto.spring.memigo.api.memigoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemigoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemigoapiApplication.class, args);
+	}
+
+}
---
+++ memigoapi/src/test/java/com/roberto/spring/memigo/api/memigoapi/MemigoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberto.spring.memigo.api.memigoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemigoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
